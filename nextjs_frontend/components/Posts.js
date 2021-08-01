2147954aa7a2bb18223be18df90a751a3c4e2377
Sofya Tuymedova <stuymedova@gmail.com>
2021-08-01 13:07:47 +0300

    chore: remove animation draft

--- nextjs_frontend/components/Posts.js
+++ nextjs_frontend/components/Posts.js
@@ -1,26 +1,17 @@
 import Link from 'next/link';
 import { parseISO, format } from 'date-fns';
 import { pad } from '../lib/utils';
-import { useTrail, animated } from '@react-spring/web';
-import { cubicOut } from 'eases-jsnext';
 
 export default function Posts({ posts }) {
-
-  const trail = useTrail(posts.length, { 
-    from: { y: 30, opacity: 0 },
-    to: { y: 0, opacity: 1 },
-    config: { duration: 600, easing: cubicOut },
-  });
-
   return (
     <>
-      {trail.map((styles, i) => {
-        const { _id, index, slug, title, excerpt, datetime } = posts[i];
+      {posts.map((post) => {
+        const { _id, index, slug, title, excerpt, datetime } = post;
 
         return (
-          <animated.div className='post-wrapper' key={_id} style={styles}>
-            <Link href={`/posts/${slug}`}>
-              <a className='post'>
+          <div className='post-wrapper' key={_id}>
+            <Link href={`/posts/${slug}`}><a>
+              <div className='post'>
                 <div className='post__id'>
                   <p>{pad(index, 3)}</p>
                 </div>
@@ -29,13 +20,13 @@ export default function Posts({ posts }) {
                   <p>{format(parseISO(datetime), 'MM.dd.yyyy')}</p>
                 </div>
 
-                <div div className='post__body'>
+                <div className='post__body'>
                   <h2 className='post__title'>{title}</h2>
                   <p className='post__excerpt'>{excerpt}</p>
                 </div>
-              </a>
-            </Link>
-          </animated.div>
+              </div>
+            </a></Link>
+          </div>
         )
       })}
     </>
